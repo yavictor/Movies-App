a79ba457f1491dd333ec2352047b6c9b2526ecb4
yavictor <yavictor37@yandex.ru>
2021-08-08 11:38:55 +0300

    Add radio change depends of searchQuery state

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,24 @@
-import { Component } from "react";
+import { Component } from 'react';
 
 class Search extends Component {
   state = {
     search: '',
-  }
+    searchQuery: '',
+  };
 
   handleKey = (e) => {
     if (e.key === 'Enter') {
       this.props.searchRequest(this.state.search);
     }
-  }
+  };
+
+  handleRadioChange = (e) => {
+    this.setState({ searchQuery: e.target.name });
+  };
 
   render() {
+    const { search, searchQuery } = this.state;
+
     return (
       <div className="searchBar">
         <div className="row">
@@ -21,37 +28,55 @@ class Search extends Component {
               placeholder="search"
               type="search"
               className="validate"
-              value={this.state.search}
-              onChange={(e) => this.setState({search: e.target.value})}
+              value={search}
+              onChange={(e) => this.setState({ search: e.target.value })}
               onKeyDown={this.handleKey}
             />
           </div>
           <button
-              className="btn col s2 input-field orange darken-2"
-              type="submit"
-              name="action"
-              onClick={() => this.props.searchRequest(this.state.search)}
-            >OK
-            </button>
-          </div>
-          <div className="row radio-group">
-            <label>
-              <input className="" name="all" type="radio" checked />
-              <span>All</span>
-            </label>
-            <label>
-              <input className="" name="films" type="radio" />
-              <span>films</span>
-            </label>
-            <label>
-              <input className="" name="series" type="radio"  />
-              <span>Series</span>
-            </label>
-          </div>      
+            className="btn col s2 input-field orange darken-2"
+            type="submit"
+            name="action"
+            onClick={() => this.props.searchRequest(search)}
+          >
+            OK
+          </button>
+        </div>
+        <div className="row radio-group">
+          <label>
+            <input
+              className=""
+              name="all"
+              type="radio"
+              checked={searchQuery === 'all'}
+              onChange={this.handleRadioChange}
+            />
+            <span>All</span>
+          </label>
+          <label>
+            <input
+              className=""
+              name="movie"
+              type="radio"
+              checked={searchQuery === 'movie'}
+              onChange={this.handleRadioChange}
+            />
+            <span>Films</span>
+          </label>
+          <label>
+            <input
+              className=""
+              name="series"
+              type="radio"
+              checked={searchQuery === 'series'}
+              onChange={this.handleRadioChange}
+            />
+            <span>Series</span>
+          </label>
+        </div>
       </div>
-
-    )
+    );
   }
 }
 
-export { Search }
+export { Search };
