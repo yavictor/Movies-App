1047bf9d5547af0332818cf1b099da7d0b03d800
yavictor <yavictor37@yandex.ru>
2021-08-13 15:13:45 +0300

    Refactor main component to hooks

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,62 +1,62 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Movies } from '../components/Movies';
 import { Search } from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 let typeFilter = '';
 
-class Main extends Component {
-  state = {
-    movies: [],
-    loading: true,
-  };
+const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
-    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=star wars`)
-      .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
-      .catch((error) => {
-        console.error(error);
-        this.setState({ loading: false });
-      });
-  }
-
-  searchRequest = (request, typeRequest = 'all') => {
-    this.setState({ loading: true });
+  const searchRequest = (request, typeRequest = 'all') => {    
+    setLoading(true);
 
     if (typeRequest !== 'all') {
       typeFilter = `&type=${typeRequest}`;
     }
     fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${request}${typeFilter}`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
       .catch((error) => {
         console.error(error);
-        this.setState({ loading: false });
+        setLoading(false);;
       });
   };
 
-  render() {
-    const { movies, loading } = this.state;
+  useEffect(() => {
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=star wars`)
+    .then((response) => response.json())
+    .then((data) => {
+      setMovies(data.Search);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error(error);
+      setLoading(false);;
+    });
+  }, []);
 
-    return (
-      <main>
-        <div className="container content">
-          <Search searchRequest={this.searchRequest} />
-          {!loading ? (
-            <Movies movies={movies} />
-          ) : (
-            <div className="">
-              <h4 className="center">LOADING...</h4>
-              <div className="progress">
-                <div className="indeterminate"></div>
-              </div>
+  return (
+    <main>
+      <div className="container content">
+        <Search searchRequest={searchRequest} />
+        {!loading ? (
+          <Movies movies={movies} />
+        ) : (
+          <div className="">
+            <h4 className="center">LOADING...</h4>
+            <div className="progress">
+              <div className="indeterminate"></div>
             </div>
-          )}
-        </div>
-      </main>
-    );
-  }
-}
+          </div>
+        )}
+      </div>
+    </main>
+  );
+};
 
 export { Main };
